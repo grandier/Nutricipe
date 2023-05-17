693bb1dd825979bf89758c1801e7b58fe9f29df6
pdamongi <damongi.yermia@gmail.com>
2023-05-17 08:53:07 +0800

    Home Activity 90%

--- Mobile Development/app/src/main/java/com/capstone/nutricipe/ui/MainActivity.kt
+++ Mobile Development/app/src/main/java/com/capstone/nutricipe/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.capstone.nutricipe
+package com.capstone.nutricipe.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.capstone.nutricipe.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
