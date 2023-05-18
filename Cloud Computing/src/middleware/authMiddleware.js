02221846302a6c2f03d9e9820967036aaa0a31cb
Ghulam Izzul Fuad <ghulam.izzul@ui.ac.id>
2023-05-18 10:07:39 +0700

    add finduser

--- Cloud Computing/src/middleware/authMiddleware.js
+++ Cloud Computing/src/middleware/authMiddleware.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const {checkUser} = require('../db/dbConfig');
 
 async function authMiddleware(req, res, next) {
     let token;
@@ -6,6 +7,10 @@ async function authMiddleware(req, res, next) {
         try{
             token = req.headers.authorization.split(' ')[1];
             const decoded = jwt.verify(token, 'hashed');
+            const user = await checkUser(decoded.userId);
+            if(!user) {
+                return res.status(401).json({error: true, message: 'Unauthorized, no user'});
+            }
             req.userID = decoded
             next();
         }
