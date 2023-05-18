50356fb4c42ad0da9254cb206ebb16513a2cf5b6
Kemas Rafly <rafly.badry20@gmail.com>
2023-05-18 20:40:04 +0700

    Login and Register with CC Finish
    Finishing Login and Register with Back-end and using token

--- Mobile Development/app/src/main/java/com/capstone/nutricipe/ui/activity/authentication/LoginActivity.kt
+++ Mobile Development/app/src/main/java/com/capstone/nutricipe/ui/activity/authentication/LoginActivity.kt
@@ -3,23 +3,119 @@ package com.capstone.nutricipe.ui.activity.authentication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
+import androidx.lifecycle.ViewModelProvider
+import com.capstone.nutricipe.R
+import com.capstone.nutricipe.data.local.Session
 import com.capstone.nutricipe.databinding.ActivityLoginBinding
 import com.capstone.nutricipe.ui.activity.MainActivity
+import com.capstone.nutricipe.ui.activity.dataStore
+import com.capstone.nutricipe.ui.customview.ButtonLogin
+import com.capstone.nutricipe.ui.customview.EmailEditText
+import com.capstone.nutricipe.ui.customview.PasswordEditText
+import com.capstone.nutricipe.ui.viewmodel.LoginViewModel
+import com.capstone.nutricipe.ui.viewmodel.ViewModelFactory
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var loginButton: ButtonLogin
+    private lateinit var emailEditText: EmailEditText
+    private lateinit var passwordEditText: PasswordEditText
+
+    private lateinit var loginViewModel: LoginViewModel
+
+    private var correctEmail = false
+    private var correctPassword = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //Uji Coba Doang
-        binding.loginButton.setOnClickListener{
-            startActivity(Intent(this, MainActivity::class.java))
+        supportActionBar?.hide()
+
+        loginButton = binding.loginButton
+        emailEditText = binding.etEmail
+        passwordEditText = binding.etPassword
+
+        // Get the user session using data store.
+        val pref = Session.getInstance(dataStore)
+
+        // Initialize the LoginViewModel
+        loginViewModel = ViewModelProvider(
+            this, ViewModelFactory(pref, this)
+        )[LoginViewModel::class.java]
+
+        if (!intent.getStringExtra("email").isNullOrEmpty()) {
+            emailEditText.setText(intent.getStringExtra("email"))
+            correctEmail = true
         }
-        binding.tvRegister.setOnClickListener{
-            startActivity(Intent(this, RegisterActivity::class.java))
+        if (!intent.getStringExtra("password").isNullOrEmpty()) {
+            passwordEditText.setText(intent.getStringExtra("password"))
+            correctPassword = true
         }
+
+        emailEditText.addTextChangedListener { text ->
+            correctEmail = !text.isNullOrEmpty() && emailRegex.matches(text.toString())
+            setLoginButtonEnable()
+        }
+
+        passwordEditText.addTextChangedListener { text ->
+            correctPassword = !text.isNullOrEmpty() && text.length >= 8
+            setLoginButtonEnable()
+        }
+
+
+        loginButton.setOnClickListener {
+            loginViewModel.login(emailEditText.text.toString(), passwordEditText.text.toString())
+        }
+
+        loginViewModel.message.observe(this) {
+            if (it == "Failure") {
+                Toast.makeText(this, R.string.login_error, Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        loginViewModel.isLoading.observe(this) {
+            showLoading(it)
+        }
+
+        loginViewModel.acceptance.observe(this) {
+            if (it) {
+                val i = Intent(this, MainActivity::class.java)
+                startActivity(i)
+                finish()
+            }
+        }
+
+        binding.tvRegister.setOnClickListener {
+            val i = Intent(this, RegisterActivity::class.java)
+            startActivity(i)
+            finish()
+        }
+    }
+
+    private fun setLoginButtonEnable() {
+        loginButton.isEnabled = correctEmail && correctPassword
+    }
+
+    private fun showLoading(state: Boolean) {
+        if (state) {
+            binding.progressBar.visibility = View.VISIBLE
+        } else {
+            binding.progressBar.visibility = View.GONE
+        }
+    }
+
+    companion object {
+        private const val ANIMATION_DURATION = 100L
+        private const val ALPHA_FULL = 1.0f
+        val emailRegex: Regex = Regex("^\\w+([.-]?\\w+)*@\\w+([.-]?\\w+)*(\\.\\w{2,3})+\$")
     }
 }
---
+++ Mobile Development/app/src/main/java/com/capstone/nutricipe/ui/viewmodel/ViewModelFactory.kt
@@ -0,0 +1,23 @@
+package com.capstone.nutricipe.ui.viewmodel
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.capstone.nutricipe.data.local.Session
+
+class ViewModelFactory(private val pref: Session, private val context: Context) :
+    ViewModelProvider.NewInstanceFactory() {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return when {
+            modelClass.isAssignableFrom(LoginViewModel::class.java) -> {
+                LoginViewModel(pref) as T
+            }
+            modelClass.isAssignableFrom(RegisterViewModel::class.java) -> {
+                RegisterViewModel(pref) as T
+            }
+            else -> throw IllegalArgumentException("Unknown Viewmodel Class: " + modelClass.name)
+        }
+    }
+
+}
