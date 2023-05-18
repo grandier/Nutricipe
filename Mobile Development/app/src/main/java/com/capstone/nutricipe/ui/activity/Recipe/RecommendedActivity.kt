5f378b22cc09c97247c15fa67ed3f82c9d07af8e
Kemas Rafly <rafly.badry20@gmail.com>
2023-05-18 08:23:43 +0700

    Finishing UI for Recipe dan Detail

---
+++ Mobile Development/app/src/main/java/com/capstone/nutricipe/ui/activity/Recipe/RecommendedActivity.kt
@@ -0,0 +1,12 @@
+package com.capstone.nutricipe.ui.activity.Recipe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.nutricipe.R
+
+class RecommendedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recommended)
+    }
+}
