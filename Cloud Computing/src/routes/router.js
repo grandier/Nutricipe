2df18e03b8e92bda374af6741c41c598aa554aad
Alfian Dorif Murtadlo <alfianitem999@gmail.com>
2023-05-18 10:55:25 +0700

    router

--- Cloud Computing/src/routes/router.js
+++ Cloud Computing/src/routes/router.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const Multer = require('multer')
 const router = express.Router();
 const register = require('../controllers/register');
 const login = require('../controllers/login');
+const authMiddleware = require('../middleware/authMiddleware')
 const handleImages = require('../controllers/handleImages');
+
+const multer = Multer({
+    storage: Multer.MemoryStorage,
+    fileSize: 5 * 1024 * 1024
+})
 
 router.post('/register', register.registerUser);
 router.post('/login', login.loginUser);
-router.post('/uploadImage', handleImages.handleUpload);
-
+router.post('/uploadImage', authMiddleware.authMiddleware, multer.single('image'), handleImages.handleUpload);
 
 module.exports = router;
