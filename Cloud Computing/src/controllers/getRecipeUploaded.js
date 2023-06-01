b388e9c8719f948a7decc6f3547622ab29dcef2f
Ghulam Izzul Fuad <ghulam.izzul@ui.ac.id>
2023-06-01 11:56:48 +0700

    synchronize api with front end

--- Cloud Computing/src/controllers/getRecipeUploaded.js
+++ Cloud Computing/src/controllers/getRecipeUploaded.js
@@ -1,23 +1,22 @@
 const { getHistoryUpload, getRecipe } = require('../db/dbConfig');
 
-
 async function getRecipeUploaded(req, res) {
   try {
     const id = req.body.idHistory;
     if (!id) {
       return res.status(400).json({ error: true, message: 'Bad Request' });
     }
-    
-    const result = await getHistoryUpload(id,);
+
+    const result = await getHistoryUpload(id);
     const recipeRes = await getRecipe(id);
-    if(!recipeRes || !result){
+    if (!recipeRes || !result) {
       throw new Error('No recipe found');
     }
 
-
-    const data = { ...result };
-    data.recipe = recipeRes;
-    return res.status(200).json({ error: false, message: 'success', data: data });
+    result.recipe = recipeRes;
+    const data = [];
+    data.push(result);
+    return res.status(200).json({ error: false, message: 'success', dataRecipe: data });
   } catch (error) {
     return res.status(500).json({ error: true, message: error.message });
   }
