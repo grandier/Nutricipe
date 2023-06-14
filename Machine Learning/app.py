9d8fd96998b23185e41f213e667d4fce32aebcab
fadelakbarputra19 <muhammad.fadel02@ui.ac.id>
2023-06-14 11:49:14 +0700

    update model and api

--- Machine Learning/app.py
+++ Machine Learning/app.py
@@ -15,8 +15,9 @@ confidence_threshold = 0.6
 @app.route(URL, methods=["POST"])
 def predict():
     global model
-    global model1
+    global model2
     labels = set()
+    my_labels = ["grape", "apple", "banana", "carrot", "cucumber", "tomato", "broccoli", "pineaple", "orange", "selada", "avocado", "cabbage","cauliflower","eggplant","kiwi","potato","strawberry"]
     if not request.method == "POST":
         return
 
@@ -26,16 +27,17 @@ def predict():
         image_bytes = response.content
         img = Image.open(io.BytesIO(image_bytes))
 
-        if model is not None and model1 is not None:
+        if model is not None or model1 is not None or model2 is not None:
             results = model(img, size=640)
-            results1 = model1(img, size=640)
+            results2 = model2(img, size=640)
             filtered_results = results.pandas().xyxy[0][results.pandas().xyxy[0]['confidence'] >= confidence_threshold]
-            filtered_results1 = results1.pandas().xyxy[0][results1.pandas().xyxy[0]['confidence'] >= confidence_threshold]
+            filtered_results2 = results2.pandas().xyxy[0][results2.pandas().xyxy[0]['confidence'] >= confidence_threshold]
             for result in filtered_results["name"]:
-                labels.add(result)
-            for result1 in filtered_results1["name"]:
-                labels.add(result1)
-
+                if result in my_labels:
+                    labels.add(result)
+            for result2 in filtered_results2["name"]:
+                if result2 in my_labels:
+                    labels.add(result2)
             return jsonify(list(labels))
         else:
             return jsonify({"error": "Model not loaded. Please check the model initialization."})
@@ -46,6 +48,7 @@ if __name__ == "__main__":
     parser.add_argument('--model', default='yolov5s', help='model to run, i.e. --model yolov5s')
     args = parser.parse_args()
 
+
     model = torch.hub.load('ultralytics/yolov5', 'custom', path='best2')
-    model1 = torch.hub.load('ultralytics/yolov5', 'custom', path='best')
+    model2 = torch.hub.load('ultralytics/yolov5', 'custom', path='best_3')
     app.run(host="0.0.0.0", port=args.port)
